ced69078df3c57eb8d0bcab92328311fcf92075c
Валерия <lerik.ru_26@mail.ru>
2017-05-05 16:36:59 +0300

    комментарии

--- Tetris/Form1.Designer.cs
+++ Tetris/Form1.Designer.cs
@@ -145,6 +145,7 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Тетрис";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
